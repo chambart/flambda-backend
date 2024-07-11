8d1dcf57539108d47283f64fb626b07828b32577
Pierre Chambart <pierre.chambart@ocamlpro.com>
2024-07-11 15:54:56 +0200

    Add Inline_param_attribute

---
+++ middle_end/flambda2/terms/inline_param_attribute.ml
@@ -0,0 +1,45 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                       Pierre Chambart, OCamlPro                        *)
+(*           Mark Shinwell and Leo White, Jane Street Europe              *)
+(*                                                                        *)
+(*   Copyright 2024 OCamlPro SAS                                          *)
+(*   Copyright 2024 Jane Street Group LLC                                 *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+type t =
+  | Inline_when_known
+  | Default_inline
+
+let [@ocamlformat "disable"] print ppf t =
+  let fprintf = Format.fprintf in
+  match t with
+  | Inline_when_known -> fprintf ppf "Inline_when_known"
+  | Default_inline -> fprintf ppf "Default_inline"
+
+let equal t1 t2 =
+  match t1, t2 with
+  | Inline_when_known, Inline_when_known
+  | Default_inline, Default_inline ->
+    true
+  | ( ( Inline_when_known
+      | Default_inline ),
+      _ ) ->
+    false
+
+let is_default t =
+  match t with
+  | Default_inline -> true
+  | Inline_when_known -> false
+
+(* let from_lambda (attr : Lambda.inline_param_attribute) = *)
+(*   match attr with *)
+(*   | Inline_when_known -> Inline_when_known *)
+(*   | Default_inline -> Default_inline *)
---
+++ middle_end/flambda2/terms/inline_param_attribute.mli
@@ -0,0 +1,29 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                       Pierre Chambart, OCamlPro                        *)
+(*           Mark Shinwell and Leo White, Jane Street Europe              *)
+(*                                                                        *)
+(*   Copyright 2024 OCamlPro SAS                                          *)
+(*   Copyright 2024 Jane Street Group LLC                                 *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+(** Function declaration inlining annotations on parameters. *)
+
+type t =
+  | Inline_when_known
+  | Default_inline
+
+val print : Format.formatter -> t -> unit
+
+val equal : t -> t -> bool
+
+val is_default : t -> bool
+
+(* val from_lambda : Lambda.inline_param_attribute -> t *)
